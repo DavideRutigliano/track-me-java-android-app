27fe0870f1e905bb2a85acbdb78454fa99be463e
DavideRutigliano <davide.ruti@gmail.com>
2018-11-26 18:31:33 +0100

    Initial code commit: started empty Spring project with SpringInitializr

---
+++ software/src/main/java/com/github/ferrantemattarutigliano/trackme/TrackmeApplication.java
@@ -0,0 +1,12 @@
+package com.github.ferrantemattarutigliano.trackme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackmeApplication.class, args);
+    }
+}
