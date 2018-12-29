36b202d4d17ddfbbfbea82c30e8de48f068b9727
DavideRutigliano <davide.ruti@gmail.com>
2018-12-29 15:35:28 +0100

    Brief: rollback to table 'user'

--- software/server/src/main/java/com/github/ferrantemattarutigliano/software/server/model/entity/User.java
+++ software/server/src/main/java/com/github/ferrantemattarutigliano/software/server/model/entity/User.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(name = "\"user\"")
 @JsonIgnoreProperties({"enabled",
         "authorities",
         "accountNonExpired",
