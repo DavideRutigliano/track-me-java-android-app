847c52f3ce4df23c76318f758162e4d54d8756e7
ClaudioFerrante95 <claudio.ferrante95@gmail.com>
2019-01-04 17:07:33 +0100

    Update with update of  RequestServiceTest.ShowIndividualDataTest

--- software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/service/RequestServiceTest.java
+++ software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/service/RequestServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.data.jpa.domain.Specification;
@@ -556,7 +557,7 @@ public class RequestServiceTest {
     }
 
     @Test
-    public void handleRequestTest_Rejected() {
+    public void showIndividualDataTest_Subscripted() {
         //create a mock user individual
         String role = Role.ROLE_INDIVIDUAL.toString();
         User mockedUser = new User("username", "password", "aa@aa.com", role);
@@ -581,6 +582,12 @@ public class RequestServiceTest {
         indRequests.add(firstIndRequest);
         //save it in thirdparty
         mockedThirdParty.setIndividualRequests(indRequests);
+        //create health data
+        Date bdate = new Date(1);
+        HealthData firstHealthData = new HealthData("high pressure", "130", bdate);
+        //add to a collection of healthdata
+        Collection<HealthData> healthDatas = new ArrayList<>();
+        healthDatas.add(firstHealthData);
 
 
 
@@ -592,12 +599,75 @@ public class RequestServiceTest {
                 .thenReturn(mockedIndividual);
         Mockito.when(mockIndividualRequestRepository.findById(0L))
                 .thenReturn(Optional.of(firstIndRequest));
+        Mockito.when(mockIndividualRequestRepository.isSubscriptionRequest(firstIndRequest.getId()))
+                .thenReturn(true);
         Mockito.when(mockIndividualRequestRepository.findByThirdParty(mockedThirdParty))
                 .thenReturn(indRequests);
+        Mockito.when(mockHealthDataRepository.findByIndividual(mockedIndividual))
+                .thenReturn(healthDatas);
 
 
-        String result = requestService.handleRequest(0L, false);
+        Collection<HealthData> result = requestService.showIndividualData(firstIndRequest);
 
-        Assert.assertEquals(Message.REQUEST_REJECTED.toString(), result);
+        Assert.assertEquals(healthDatas, result);
+    }
+
+    @Test
+    public void showIndividualDataTest_NotSubscripted() {
+        //create a mock user individual
+        String role = Role.ROLE_INDIVIDUAL.toString();
+        User mockedUser = new User("username", "password", "aa@aa.com", role);
+        Individual mockedIndividual = new Individual();
+        mockedIndividual.setUser(mockedUser);
+        mockedIndividual.setFirstname("pippo");
+        mockedIndividual.setLastname("pippetti");
+        mockedIndividual.setSsn("999999999");
+        //create mock user thridparty
+        String role2 = Role.ROLE_THIRD_PARTY.toString();
+        User mockedUser2 = new User("Username", "Password", "AA@AA.com", role);
+        ThirdParty mockedThirdParty = new ThirdParty();
+        mockedThirdParty.setUser(mockedUser2);
+        mockedThirdParty.setVat("11111111111");
+        mockedThirdParty.setOrganizationName("topolino");
+        //create individual requests
+        IndividualRequest firstIndRequest = createMockIndRequest(mockedIndividual.getSsn());
+        firstIndRequest.setId(0L);
+        firstIndRequest.setThirdParty(mockedThirdParty);
+        Date reqDate = new Date(98, 10, 01);
+        firstIndRequest.setDate(reqDate);
+        //add request to a collection
+        Collection<IndividualRequest> indRequests = new ArrayList<>();
+        indRequests.add(firstIndRequest);
+        //save it in thirdparty
+        mockedThirdParty.setIndividualRequests(indRequests);
+        //create health data
+        Date bdate = new Date(96, 12, 01);
+        HealthData firstHealthData = new HealthData("high pressure", "130", bdate);
+        //add to a collection of healthdata
+        Collection<HealthData> healthDatas = new ArrayList<>();
+        healthDatas.add(firstHealthData);
+
+
+
+
+        /* TEST STARTS HERE */
+        mockIndividualAuthorized(mockedUser, mockedIndividual);
+
+
+        Mockito.when(mockIndividualRepository.findBySsn(mockedIndividual.getSsn()))
+                .thenReturn(mockedIndividual);
+        Mockito.when(mockIndividualRequestRepository.findById(0L))
+                .thenReturn(Optional.of(firstIndRequest));
+        Mockito.when(mockIndividualRequestRepository.isSubscriptionRequest(firstIndRequest.getId()))
+                .thenReturn(false);
+        Mockito.when(mockIndividualRequestRepository.findByThirdParty(mockedThirdParty))
+                .thenReturn(indRequests);
+        Mockito.when(mockHealthDataRepository.findUntilTimestamp(firstIndRequest.getSsn(), firstIndRequest.getDate(), firstIndRequest.getTime()))
+                .thenReturn(healthDatas);
+
+
+        Collection<HealthData> result = requestService.showIndividualData(firstIndRequest);
+
+        Assert.assertEquals(healthDatas, result);
     }
 }
