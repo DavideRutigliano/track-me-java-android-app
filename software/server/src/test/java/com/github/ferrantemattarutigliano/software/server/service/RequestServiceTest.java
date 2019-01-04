e8bc2cb3f2ba361431625161e278949bebd34e71
ClaudioFerrante95 <claudio.ferrante95@gmail.com>
2019-01-04 16:26:20 +0100

    Update with update of  RequestServiceTest.HandleRequestTest

--- software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/service/RequestServiceTest.java
+++ software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/service/RequestServiceTest.java
@@ -510,7 +510,7 @@ public class RequestServiceTest {
     }
 
     @Test
-    public void handleRequestTest() {
+    public void handleRequestTest_Accepted() {
         //create a mock user individual
         String role = Role.ROLE_INDIVIDUAL.toString();
         User mockedUser = new User("username", "password", "aa@aa.com", role);
@@ -553,5 +553,51 @@ public class RequestServiceTest {
         String result = requestService.handleRequest(0L, true);
 
         Assert.assertEquals(Message.REQUEST_ACCEPTED.toString(), result);
+    }
+
+    @Test
+    public void handleRequestTest_Rejected() {
+        //create a mock user individual
+        String role = Role.ROLE_INDIVIDUAL.toString();
+        User mockedUser = new User("username", "password", "aa@aa.com", role);
+        Individual mockedIndividual = new Individual();
+        mockedIndividual.setUser(mockedUser);
+        mockedIndividual.setFirstname("pippo");
+        mockedIndividual.setLastname("pippetti");
+        mockedIndividual.setSsn("999999999");
+        //create mock user thridparty
+        String role2 = Role.ROLE_THIRD_PARTY.toString();
+        User mockedUser2 = new User("Username", "Password", "AA@AA.com", role);
+        ThirdParty mockedThirdParty = new ThirdParty();
+        mockedThirdParty.setUser(mockedUser2);
+        mockedThirdParty.setVat("11111111111");
+        mockedThirdParty.setOrganizationName("topolino");
+        //create individual requests
+        IndividualRequest firstIndRequest = createMockIndRequest(mockedIndividual.getSsn());
+        firstIndRequest.setId(0L);
+        firstIndRequest.setThirdParty(mockedThirdParty);
+        //add request to a collection
+        Collection<IndividualRequest> indRequests = new ArrayList<>();
+        indRequests.add(firstIndRequest);
+        //save it in thirdparty
+        mockedThirdParty.setIndividualRequests(indRequests);
+
+
+
+        /* TEST STARTS HERE */
+        mockIndividualAuthorized(mockedUser, mockedIndividual);
+
+
+        Mockito.when(mockIndividualRepository.findBySsn(mockedIndividual.getSsn()))
+                .thenReturn(mockedIndividual);
+        Mockito.when(mockIndividualRequestRepository.findById(0L))
+                .thenReturn(Optional.of(firstIndRequest));
+        Mockito.when(mockIndividualRequestRepository.findByThirdParty(mockedThirdParty))
+                .thenReturn(indRequests);
+
+
+        String result = requestService.handleRequest(0L, false);
+
+        Assert.assertEquals(Message.REQUEST_REJECTED.toString(), result);
     }
 }
