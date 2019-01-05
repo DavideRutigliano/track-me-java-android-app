95bd077611da1b2e939377e5c2bcabf0079a616d
ClaudioFerrante95 <claudio.ferrante95@gmail.com>
2019-01-05 12:53:05 +0100

    Update RunControllerTest with ShowEnrolledRunsTest

--- software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/controller/RunControllerTest.java
+++ software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/controller/RunControllerTest.java
@@ -296,5 +296,53 @@ public class RunControllerTest {
 
     }
 
+    @Test
+    public void showEnrolledRunsTest() {
+        // RunDTO creation
+        RunDTO firstRunDTO = createMockRunDTO();
+        //create a mock user
+        String role = Role.ROLE_INDIVIDUAL.toString();
+        User mockedUser = new User("username", "password", "aa@aa.com", role);
+        Individual mockedIndividual = new Individual();
+        mockedIndividual.setUser(mockedUser);
+        mockedIndividual.setFirstname("pippo");
+        mockedIndividual.setLastname("pippetti");
+        //create mock Run
+        Run firstRun = createMockRun(mockedIndividual, "10.0", "50.0");
+        Run secondRun = createMockRun(mockedIndividual, "20.0", "20.0");
+        firstRun.setState("created");
+        secondRun.setState("created");
+        //add to a collection
+        Collection<Run> orgRuns = new ArrayList<>();
+        orgRuns.add(firstRun);
+        orgRuns.add(secondRun);
+        //add to organizer and run enrolled
+        mockedIndividual.setCreatedRuns(orgRuns);
+        mockedIndividual.setEnrolledRuns(orgRuns);
+
+
+        //TEST STARTS HERE
+
+        Mockito.when(mockRunService.showEnrolledRuns())
+                .thenReturn(orgRuns);
+
+        Collection<RunDTO> result = runController.showEnrolledRuns();
+
+        Collection<RunDTO> expRuns = convertRuns(orgRuns);
+        Iterator<RunDTO> RDE = expRuns.iterator();
+        for (Iterator<RunDTO> i = result.iterator(); i.hasNext(); ) {
+            RunDTO RD = i.next();
+            RunDTO h = RDE.next();
+            h.setPath(RD.getPath());
+            Assert.assertEquals(RD.getId(), h.getId());
+            Assert.assertEquals(RD.getPath(), h.getPath());
+            Assert.assertEquals(RD.getDate(), h.getDate());
+            Assert.assertEquals(RD.getTime(), h.getTime());
+            Assert.assertEquals(RD.getState(), h.getState());
+            Assert.assertEquals(RD.getTitle(), h.getTitle());
+        }
+
+    }
+
 
 }
