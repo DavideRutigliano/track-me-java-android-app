210f2138b56742390acb5bcc992c8e3d8966bd0e
ClaudioFerrante95 <claudio.ferrante95@gmail.com>
2019-01-05 16:51:09 +0100

    Update with  RequestControllerTest with showSentGroupRequestTest

--- software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/controller/RequestControllerTest.java
+++ software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/controller/RequestControllerTest.java
@@ -229,6 +229,44 @@ public class RequestControllerTest {
         Collection<IndividualRequest> result = requestController.showSentIndividualRequest();
 
         Assert.assertEquals(indRequests, result);
+
+    }
+
+    @Test
+    public void showSentGroupRequestTest() {
+        //create a mock user individual
+        String role = Role.ROLE_INDIVIDUAL.toString();
+        User mockedUser = new User("username", "password", "aa@aa.com", role);
+        Individual mockedIndividual = new Individual();
+        mockedIndividual.setUser(mockedUser);
+        mockedIndividual.setFirstname("pippo");
+        mockedIndividual.setLastname("pippetti");
+        mockedIndividual.setSsn("999999999");
+        //create mock user thridparty
+        String role2 = Role.ROLE_THIRD_PARTY.toString();
+        User mockedUser2 = new User("Username", "Password", "AA@AA.com", role);
+        ThirdParty mockedThirdParty = new ThirdParty();
+        mockedThirdParty.setUser(mockedUser2);
+        mockedThirdParty.setVat("11111111111");
+        mockedThirdParty.setOrganizationName("topolino");
+        //create group requests
+        GroupRequest firstGroupRequest = createMockGroupRequest("state=italy");
+        firstGroupRequest.setSubscription(false);
+        //add request to a collection
+        Collection<GroupRequest> groupRequests = new ArrayList<>();
+        groupRequests.add(firstGroupRequest);
+        //save it in thirdparty
+        mockedThirdParty.setGroupRequests(groupRequests);
+        List<Individual> indCol = new ArrayList<>();
+        indCol.add(mockedIndividual);
+
+        /* TEST STARTS HERE */
+
+        Mockito.when(mockRequestService.showSentGroupRequest())
+                .thenReturn(groupRequests);
+        Collection<GroupRequest> result = requestController.showSentGroupRequest();
+
+        Assert.assertEquals(groupRequests, result);
     }
 
 
