a87d268019581f6b416f390993d38e47bbef2e22
DavideRutigliano <davide.ruti@gmail.com>
2019-01-06 22:33:21 +0100

    health data service

--- software/client/app/src/main/java/com/github/ferrantemattarutigliano/software/client/fragment/individual/IndividualInsertDataFragment.java
+++ software/client/app/src/main/java/com/github/ferrantemattarutigliano/software/client/fragment/individual/IndividualInsertDataFragment.java
@@ -1,36 +1,23 @@
 package com.github.ferrantemattarutigliano.software.client.fragment.individual;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.github.ferrantemattarutigliano.software.client.R;
-import com.github.ferrantemattarutigliano.software.client.presenter.individual.IndividualInsertDataPresenter;
-import com.github.ferrantemattarutigliano.software.client.view.individual.IndividualInsertDataView;
-import com.google.android.gms.wearable.DataClient;
-import com.google.android.gms.wearable.DataEvent;
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.DataMapItem;
-import com.google.android.gms.wearable.Wearable;
-
-import java.sql.Date;
-import java.sql.Time;
+import com.github.ferrantemattarutigliano.software.client.service.SendHealthDataService;
 
 
-public class IndividualInsertDataFragment extends Fragment implements
-        IndividualInsertDataView,
-        DataClient.OnDataChangedListener {
+public class IndividualInsertDataFragment extends Fragment {
 
-    private IndividualInsertDataPresenter individualInsertDataPresenter;
-    private TextView messageContainer;
-    String datapath = "/data_path";
     private Button deviceButton;
+
+    private SendHealthDataService sendHealthDataService;
+    private Intent sendHealthDataIntent;
 
     public IndividualInsertDataFragment() {
         // Required empty public constructor
@@ -39,68 +26,25 @@ public class IndividualInsertDataFragment extends Fragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        individualInsertDataPresenter = new IndividualInsertDataPresenter(this);
-        Wearable.getDataClient(getContext()).addListener(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        individualInsertDataPresenter = new IndividualInsertDataPresenter(this);
 
-        View v = inflater.inflate(R.layout.fragment_individual_connect_external_device, container, false);
-        messageContainer = v.findViewById(R.id.container_individual_external_device_messages);
-        deviceButton = v.findViewById(R.id.button_individual_connect_external_device);
+        sendHealthDataService = new SendHealthDataService();
+        sendHealthDataIntent = new Intent(getActivity(), sendHealthDataService.getClass());
 
-        Wearable.getDataClient(getContext()).addListener(this);
+        View v = inflater.inflate(R.layout.fragment_individual_insert_data, container, false);
+        deviceButton = v.findViewById(R.id.button_individual_send_data);
 
         deviceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String messages = messageContainer.getText().toString();
-                individualInsertDataPresenter.doInsertData(messages);
+                getActivity().startService(sendHealthDataIntent);
             }
         });
 
         return v;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Wearable.getDataClient(getContext()).addListener(this);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Wearable.getDataClient(getContext()).removeListener(this);
-    }
-
-    @Override
-    public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
-        for (DataEvent event : dataEventBuffer) {
-            if (event.getType() == DataEvent.TYPE_CHANGED) {
-                String path = event.getDataItem().getUri().getPath();
-                if (datapath.equals(path)) {
-                    DataMapItem dataMapItem = DataMapItem.fromDataItem(event.getDataItem());
-                    String message = dataMapItem.getDataMap().getString("message");
-                    java.util.Date date = new java.util.Date();
-                    messageContainer.append(message + ". Received at: " + new Date(date.getTime()) + "_" + new Time(date.getTime()) + "\n");
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onInsertDataSuccess(String message) {
-        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
-        messageContainer.setText("");
-    }
-
-    @Override
-    public void onInsertDataFailure(String message) {
-        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
-        messageContainer.setText("");
     }
 }
--- software/client/app/src/main/java/com/github/ferrantemattarutigliano/software/client/service/SendHealthDataService.java
+++ software/client/app/src/main/java/com/github/ferrantemattarutigliano/software/client/service/SendHealthDataService.java
@@ -1,4 +1,81 @@
 package com.github.ferrantemattarutigliano.software.client.service;
 
-public class SendHealthDataService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import com.github.ferrantemattarutigliano.software.client.httprequest.AsyncResponse;
+import com.github.ferrantemattarutigliano.software.client.model.HealthDataDTO;
+import com.github.ferrantemattarutigliano.software.client.task.individual.IndividualInsertDataTask;
+import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.sql.Time;
+import java.util.Collections;
+
+public class SendHealthDataService extends Service implements DataClient.OnDataChangedListener {
+
+    private static final String datapath = "/data_path";
+
+    public SendHealthDataService() {
+        super();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        Wearable.getDataClient(getApplicationContext()).addListener(this);
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
+        for (DataEvent event : dataEventBuffer) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                String path = event.getDataItem().getUri().getPath();
+                if (datapath.equals(path)) {
+                    DataMapItem dataMapItem = DataMapItem.fromDataItem(event.getDataItem());
+                    String message = dataMapItem.getDataMap().getString("message");
+                    doInsertData(message);
+                }
+            }
+        }
+    }
+
+    private void doInsertData(String message) {
+        String name = StringUtils.substringBefore(message, ":");
+        String value = StringUtils.substringBetween(message, ":", ". Received at: ");
+        HealthDataDTO healthDataDTO = new HealthDataDTO();
+        healthDataDTO.setName(name);
+        healthDataDTO.setValue(value);
+        java.util.Date date = new java.util.Date();
+        healthDataDTO.setDate(new java.sql.Date(date.getTime()));
+        healthDataDTO.setTime(new Time(date.getTime()));
+
+        new IndividualInsertDataTask(Collections.singleton(healthDataDTO), new AsyncResponse<String>() {
+            @Override
+            public void taskFinish(String message) {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void taskFailMessage(String message) {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        }).execute();
+    }
 }
