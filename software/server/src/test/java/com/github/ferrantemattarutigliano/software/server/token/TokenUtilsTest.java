94b5aab405b0c8ea3aafce5e42da14f3148920c1
ClaudioFerrante95 <claudio.ferrante95@gmail.com>
2019-01-10 18:52:04 +0100

    Insert Token Utils Test

---
+++ software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/token/TokenUtilsTest.java
@@ -0,0 +1,78 @@
+package com.github.ferrantemattarutigliano.software.server.token;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.token.KeyBasedPersistenceTokenService;
+import org.springframework.security.core.token.Token;
+import org.springframework.security.core.token.TokenService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.SecureRandom;
+
+import static org.mockito.ArgumentMatchers.any;
+
+public class TokenUtilsTest {
+    @InjectMocks
+    private TokenUtils tokenUtils;
+
+    @Mock
+    private TokenService mockTokenService;
+
+    @Mock
+    private HttpServletResponse mockResponse;
+
+    @Mock
+    private MockHttpServletRequest mockRequest1;
+
+
+    @Before
+    public void initTest() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getUsernameAndPassFromTokenTest() {
+
+        final String HEADER_SECURITY_TOKEN = "X-Auth-Token";
+        final Integer SERVER_INTEGER = 32;
+        final String SERVER_SECRET = "#SèRv3r_$3kRet:=Tr4CkM3_s.SécR37";
+        final Integer PRN_BYTES = 16;
+        final KeyBasedPersistenceTokenService tokenService;
+
+        tokenService = new KeyBasedPersistenceTokenService();
+        tokenService.setServerInteger(SERVER_INTEGER);
+        tokenService.setServerSecret(SERVER_SECRET);
+        tokenService.setPseudoRandomNumberBytes(PRN_BYTES);
+        tokenService.setSecureRandom(new SecureRandom());
+
+        String key = tokenService.allocateToken("username" + ":" + "password").getKey();
+
+
+        Mockito.when(mockRequest1.getHeader("X-Auth-Token"))
+                .thenReturn(key);
+
+        String[] ExRes = new String[2];
+        ExRes[0] = "username";
+        ExRes[1] = "password";
+
+
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.addHeader("X-Auth-Token", key);
+
+
+        String[] result = tokenUtils.getUsernameAndPassFromToken(mockHttpServletRequest);
+
+        Assert.assertEquals(ExRes[0], result[0]);
+        Assert.assertEquals(ExRes[1], result[1]);
+    }
+}
+
