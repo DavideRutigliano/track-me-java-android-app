ccaff331da29da2846939cdec9d04e3b9785f751
DavideRutigliano <davide.ruti@gmail.com>
2019-01-11 18:07:51 +0100

    configuration classes test

--- software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/ServerApplicationTest.java
+++ software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/ServerApplicationTest.java
@@ -1,6 +1,4 @@
-/*
 package com.github.ferrantemattarutigliano.software.server;
-
 
 import com.github.ferrantemattarutigliano.software.server.config.SecurityConfig;
 import com.github.ferrantemattarutigliano.software.server.config.WebSocketConfig;
@@ -10,14 +8,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration({})
+@SpringBootTest(classes = ServerApplication.class,
+                webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = {SecurityConfig.class,
+                                WebSocketConfig.class})
 public class ServerApplicationTest {
     @Test
     public void contextLoads() {
         ServerApplication.main(new String[] {});
     }
 }
-*/
