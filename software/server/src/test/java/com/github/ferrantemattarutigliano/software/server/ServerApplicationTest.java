8a3a94e5b7068ef380d311c6a6d18e9199051c68
ClaudioFerrante95 <claudio.ferrante95@gmail.com>
2019-01-11 17:08:26 +0100

    Server Config Test

--- software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/ServerApplicationTest.java
+++ software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/ServerApplicationTest.java
@@ -1,18 +1,22 @@
-/*
+
 package com.github.ferrantemattarutigliano.software.server;
 
 
+import com.github.ferrantemattarutigliano.software.server.config.SecurityConfig;
+import com.github.ferrantemattarutigliano.software.server.config.WebSocketConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = {SecurityConfig.class, WebSocketConfig.class})
 public class ServerApplicationTest {
     @Test
     public void contextLoads() {
         ServerApplication.main(new String[] {});
     }
 }
-*/
