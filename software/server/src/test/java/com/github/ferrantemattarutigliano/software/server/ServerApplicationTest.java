3aa6be52bc1e139beb9ac7d044422a50adce6046
DavideRutigliano <davide.ruti@gmail.com>
2019-01-11 18:39:17 +0100

    config test

--- software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/ServerApplicationTest.java
+++ software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/ServerApplicationTest.java
@@ -7,8 +7,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ServerApplication.class,
@@ -16,6 +20,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(value = "classpath:application.properties")
 @ContextConfiguration(classes = {SecurityConfig.class,
                                 WebSocketConfig.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+                        DirtiesContextTestExecutionListener.class,
+                        TransactionalTestExecutionListener.class})
 public class ServerApplicationTest {
     @Test
     public void contextLoads() {
