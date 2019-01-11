b611832958982621eeb862f9e2caf7d85d4e379a
Davide Rutigliano <37795483+DavideRutigliano@users.noreply.github.com>
2019-01-11 16:03:11 +0100

    Update ServerApplicationTest.java

--- software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/ServerApplicationTest.java
+++ software/server/src/test/java/com/github/ferrantemattarutigliano/software/server/ServerApplicationTest.java
@@ -4,12 +4,10 @@ package com.github.ferrantemattarutigliano.software.server;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(classes = ServerApplication.class)
 public class ServerApplicationTest {
     @Test
     public void contextLoads() {
